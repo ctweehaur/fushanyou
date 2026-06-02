b2af8f9bd999e9e944fbb9cce751c0935e63b2f8
ctweehaur <ctweehaur@gmail.com>
2026-06-02 17:36:09 +0800

    Create fushanyou_translation.js

---
+++ fushanyou_translation.js
@@ -0,0 +1,17 @@
+const translationData = [
+    {
+        zh: "有一个路过江边的人，看见一个人正牵拉着一个婴儿，想要把他扔进大江中，婴儿吓得大声哭泣。路人便上前询问他这么做的原因。",
+        py: "yǒu yī gè lù guò jiāng biān de rén jiàn qǐ yī gè rén fāng qiān lā zhe yī gè yīng ér xiǎng yào bǎ tā rēng jìn dà jiāng zhōng yīng ér xià dé dà shēng kū qì lù rén biàn shàng qián xún wèn tā zhè me zuò de yuán yīn",
+        en: "A person passing by the river saw a man pulling a baby, attempting to throw him into the deep river. The baby was crying loudly in terror. The passerby rushed forward to ask for the reason."
+    },
+    {
+        zh: "那个人回答说：“这个孩子的父亲非常擅长游泳。”",
+        py: "nà gè rén huí dá shuō zhè gè hái zǐ de fù qīn fēi cháng shàn cháng yóu yǒng",
+        en: "The man replied, 'This baby's father is a very expert swimmer!'"
+    },
+    {
+        zh: "孩子的父亲虽然擅长游泳，但他的儿子难道一出生也就立刻擅长游泳了吗？凭借这种荒谬的逻辑来处理世间的事物，也必然会把事情搞得颠倒荒谬、彻底失败。",
+        py: "hái zǐ de fù qīn suī rán shàn cháng yóu yǒng dàn tā de ér zǐ nán dào yī chū shēng yě jiù lì kè shàn cháng yóu yǒng le ma píng jiè zhè zhǒng huāng miù de luó jí lái chǔ lǐ shì jiān de shì wù yě bì rán huì bǎ shì qíng gǎo dé diān dǎo huāng miù chè dǐ shī bài",
+        en: "Although the father is a skilled swimmer, does that automatically mean his newborn son is instantly a skilled swimmer too? Managing worldly affairs with such ridiculous logic will inevitably lead to utter failure and absurdity."
+    }
+];
